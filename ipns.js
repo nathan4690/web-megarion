07299b4be192423b00e0fba49f5f0d620179092c
Megarion <andy181209x@gmail.com>
2021-07-15 15:32:21 +0700

    tiny

--- ipns.js
+++
@@ -1,7 +0,0 @@
-var slider = document.getElementById("phone");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
-
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
